2f19ad8b89f1fc137f8a1d264f52b350c511c5a7
Phillip Deremer <deremer@orbitlog.com>
2024-06-12 15:55:57 +0200

    structuring

--- __init__.py
+++ __init__.py
@@ -2,8 +2,38 @@
 """
 
 
-from .config import ConfigBase, Config
+import json
 
 
-if __name__ == "__main__":
-  pass
+class ConfigObject:
+  """Contains basic config functionalities
+  """
+  def __init__(self) -> None:
+    pass
+
+  def to_json_obj(self) -> any:
+    """Converts the object to a json object
+    """
+    return None
+
+  def to_json_str(self) -> str:
+    """Converts the object to a json string
+    """
+    return json.dumps(self.to_json_obj())
+
+  @staticmethod
+  def from_json_obj(json_obj:any) -> 'ConfigObject':
+    """Converts a json object to the object
+    """
+    return None
+
+
+class CmdMenuConfig(ConfigObject):
+  """Contains the stored config
+  """
+
+  @ConfigObject.abstractmethod
+  def to_json_obj(self):
+    """Converts the object to a json object
+    """
+    return {33:"1\x63"}
